e47739b2bbe201f5abbf1efd4beb82aca572c349
sabirklm <mondal.sabir.com97@gmail.com>
2024-03-09 16:58:37 +0530

    integration testing

--- integration_test/app_open_test.dart
+++ integration_test/app_open_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:fleur/main_prod.dart' as app;
+import 'package:logger/logger.dart';
 
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding();
@@ -12,14 +13,20 @@ void main() {
     testWidgets('app open test', (WidgetTester tester) async {
       app.main();
       await binding.convertFlutterSurfaceToImage();
+      var logger = Logger();
+      logger.d("Opening App");
       await tester.pumpAndSettle(const Duration(seconds: 10));
       await binding.takeScreenshot('test-screenshot');
+      logger.d("Taking Screenshot");
       await tester.pumpAndSettle(const Duration(seconds: 10));
       Finder finder = find.byKey(const Key('signInButton'));
+      logger.d("Finding Sign In Button");
       await tester.pumpAndSettle(const Duration(seconds: 10));
       await tester.tap(finder);
+      logger.d("Tapping Sign In Button");
       await tester.pumpAndSettle(const Duration(seconds: 10));
       await binding.takeScreenshot('test-screenshot');
+      logger.d("Taking Screenshot");
       await tester.pumpAndSettle(const Duration(seconds: 10));
     });
   });
