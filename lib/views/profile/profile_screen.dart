982c8325f38ca527532b028fa013c32a5ec88bcf
sabirklm <mondal.sabir.com97@gmail.com>
2024-03-30 15:07:55 +0530

    Minor enhancements

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:fleur/bloc/user_bloc.dart';
 import 'package:fleur/utills/sanckbar.dart';
 import 'package:fleur/views/profile/account_information_screen.dart';
+import 'package:fleur/views/profile/my_vehicle_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -54,6 +55,15 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     context,
                     MaterialPageRoute(
                       builder: (context) => const AccountInformationScreen(),
+                    ),
+                  );
+                  return;
+                }
+                if (profileItems[index].type == "my_car") {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const MyVehicleScreen(),
                     ),
                   );
                   return;
