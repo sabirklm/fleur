d2a366fe5af74ffd1c10632c2ea955b5cd2574e5
sabirklm <mondal.sabir.com97@gmail.com>
2024-04-06 14:43:38 +0530

    Minor enhancements

--- lib/models/about_us.dart
+++ lib/models/about_us.dart
@@ -20,6 +20,54 @@ class AboutUs {
   });
 }
 
+class PrivacyPolicy {
+  final String? companyName;
+  final String? privacyDescription;
+  final String? informationCollection;
+  final String? informationUsage;
+  final String? informationProtection;
+  final String? cookieUsage;
+  final String? dataRetention;
+  final String? yourRights;
+  final String? changesToPolicy;
+  final String? contactInformation;
+
+  PrivacyPolicy({
+    this.companyName,
+    this.privacyDescription,
+    this.informationCollection,
+    this.informationUsage,
+    this.informationProtection,
+    this.cookieUsage,
+    this.dataRetention,
+    this.yourRights,
+    this.changesToPolicy,
+    this.contactInformation,
+  });
+}
+
+var mockPrivacyPolicy = PrivacyPolicy(
+  companyName: "Fleeur",
+  privacyDescription:
+      "At Fleeur, we are committed to protecting the privacy and security of our users. This Privacy Policy outlines how we collect, use, protect, and retain your personal information.",
+  informationCollection:
+      "We collect information you provide directly to us, such as when you create an account, submit a query, or interact with our services. We may also collect information automatically when you use our website or mobile app, such as your IP address, device information, and browsing activity.",
+  informationUsage:
+      "We use the information we collect to provide, maintain, and improve our services, personalize content, communicate with you, and fulfill your requests. We may also use your information to detect, prevent, and investigate fraudulent or illegal activities.",
+  informationProtection:
+      "We take measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic storage is completely secure, so we cannot guarantee absolute security.",
+  cookieUsage:
+      "We use cookies and similar technologies to analyze trends, administer our website, track users' movements, and gather demographic information. You can control cookies through your browser settings and opt out of certain types of tracking.",
+  dataRetention:
+      "We retain your personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy, unless a longer retention period is required or permitted by law.",
+  yourRights:
+      "You have the right to access, update, or delete your personal information. You can also object to the processing of your information or request restrictions on its use. Please contact us if you have any questions or concerns about your privacy rights.",
+  changesToPolicy:
+      "We may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements. We will notify you of any material changes and obtain your consent if required by law.",
+  contactInformation:
+      "If you have any questions, comments, or concerns about our Privacy Policy or practices, please contact us at privacy@fleeur.com.",
+);
+
 var mockAboutUs = AboutUs(
   companyName: "Fleeur",
   companyDescription:
--- lib/views/profile/privacy_policy_view.dart
+++ lib/views/profile/privacy_policy_view.dart
@@ -1,4 +1,6 @@
+import 'package:fleur/models/about_us.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PrivacyPolicyView extends StatelessWidget {
   const PrivacyPolicyView({super.key});
@@ -9,8 +11,61 @@ class PrivacyPolicyView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Privacy Policy'),
       ),
-      body: const Center(
-        child: Text('This is the privacy policy page.'),
+      body: ListView(
+        children: [
+          FutureBuilder(
+            future: Future.delayed(Duration(seconds: 3)),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      mockPrivacyPolicy.companyName ?? "",
+                      style: GoogleFonts.sen(
+                        fontSize: 22,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      mockPrivacyPolicy.privacyDescription ?? "",
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      mockPrivacyPolicy.cookieUsage ?? "",
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      mockPrivacyPolicy.informationCollection ?? "",
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      mockPrivacyPolicy.yourRights ?? "",
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      mockPrivacyPolicy.changesToPolicy ?? "",
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      mockPrivacyPolicy.contactInformation ?? "",
+                    ),
+                    const SizedBox(height: 8),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
